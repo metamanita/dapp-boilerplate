f128d85084a44c93d8b53667ecf6dacdf212c127
Jumaru <spiralout.eu@gmail.com>
2022-11-01 19:44:42 +0200

    reduce drawer height

--- src/components/Layouts.tsx
+++ src/components/Layouts.tsx
@@ -12,10 +12,12 @@ export default function Layout({ children }: Props) {
       flexDirection="column"
       // alignItems="center"
       justifyContent="center"
-      h="100vh"
+      // h="100vh"
       bg="gray.800"
     >
       {children}
+         
     </Flex>
+
   )
 }
