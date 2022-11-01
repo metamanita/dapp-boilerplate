29a8895bb853860c13140970c3ea3ee011b16e19
Jumaru <spiralout.eu@gmail.com>
2022-11-01 19:41:47 +0200

    clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,25 @@
 import './App.css';
-import { ChakraProvider, Switch } from "@chakra-ui/react";
+import { ChakraProvider} from "@chakra-ui/react";
 import Layout from './components/Layouts';
 import NavigationBar from './components/NavigationBar';
 import theme from './theme'
-import { BrowserRouter, Outlet, Route } from "react-router-dom";
-import Welcome from './routes/Welcome';
-import Account from './routes/Account';
+import { BrowserRouter} from "react-router-dom";
 import AppRouter from './AppRouter';
 
 function App() {
-  
+
   return (
     <ChakraProvider theme={theme}>
       <Layout>
-      <BrowserRouter>
-        <NavigationBar>
-        
-        <AppRouter />
-        </NavigationBar>
+        <BrowserRouter>
+          <NavigationBar>
+            <AppRouter />
+          </NavigationBar>
+          
         </BrowserRouter>
         
-      
       </Layout>
-
+       
     </ChakraProvider>
   );
 }
